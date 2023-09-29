0152069871302f72320108b987f87feedb37f324
KhizarAziz <khizar@scanbot.io>
2023-09-29 23:57:16 +0200

    added the requirements and configs into repo

--- preprocessing/preprocess_yelp_review_dataset.py
+++ preprocessing/preprocess_yelp_review_dataset.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from datasets import Dataset
 import logging
+import re
 
 # Text cleaning function (customize as needed)
 def clean_text(text):
@@ -60,19 +61,33 @@ def preprocess_data(df):
 def dump_to_s3(train_data,val_data,train_dumpyard_path,val_dumpyard_path):
     # dump data into S3
     # Using Hugging Face's Datasets
-    train_data.save_to_disk() # "./datasets/train_data/"
+    train_data.save_to_disk(train_dumpyard_path) # "./datasets/train_data/"
     print('Saved Training data: ',train_dumpyard_path)
-    val_data.save_to_disk() # "./datasets/val_data/"
+    val_data.save_to_disk(val_dumpyard_path) # "./datasets/val_data/"
     print('Saved Validation data: ',val_dumpyard_path)
+
+import argparse
+
 
 
 if __name__ == "__main__":
-    dataset_path = 'your_dataset_path_here'
-    chunk_size = 1000  # Replace with your actual chunk size
 
-    df = load_dataset(dataset_path, chunk_size)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', required=True)
+    parser.add_argument('--chunk_size', type=int, required=True)
+    args = parser.parse_args()
+
+    df = load_dataset(args.dataset_path, args.chunk_size)
+    print("Dataset Loaded!")
     if df is not None:
         clean_data(df)
+        print("Dataset Cleaned!")
         feature_engineering(df)
+        print("Dataset Engineered!")
         train_data, val_data = preprocess_data(df)
+        print("Dataset Cleaned!")
+
+        # try to dump to s3
+        train_dumpyard_path = '/'.join(args.dataset_path.split('/')[:-1]) + '/train_set/'
+        val_dumpyard_path = '/'.join(args.dataset_path.split('/')[:-1]) + '/val_set/'
         dump_to_s3(train_data,val_data,train_dumpyard_path,val_dumpyard_path)
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, Trainer, TrainingArguments
 from datasets import load_from_disk
 import logging
 
@@ -20,6 +20,7 @@ def load_preprocessed_data(train_path, val_path):
 
 def tokenize_data(train_data, val_data, tokenize_function):
     try:
+        print("Tokenizing Dataset....")
         train_data_tokenized = train_data.map(tokenize_function, batched=True)
         val_data_tokenized = val_data.map(tokenize_function, batched=True)
         return train_data_tokenized, val_data_tokenized
@@ -33,9 +34,18 @@ def set_pytorch_format(data):
     except Exception as e:
         logging.error(f"Failed to set PyTorch format: {e}")
 
+import argparse
+import yaml
+
+
 if __name__ == "__main__":
-    train_path = "path/to/directory_train"
-    val_path = "path/to/directory_val"
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_data_path', required=True)
+    parser.add_argument('--val_data_path', required=True)
+    args = parser.parse_args()
+
+    train_path = args.train_data_path
+    val_path = args.val_data_path
 
     train_data, val_data = load_preprocessed_data(train_path, val_path)
     if train_data and val_data:
@@ -44,14 +54,19 @@ if __name__ == "__main__":
         set_pytorch_format(train_data_tokenized)
         set_pytorch_format(val_data_tokenized)
 
+
+        # Load the YAML config
+        with open("training_config.yaml", 'r') as stream:
+            config = yaml.safe_load(stream)
+        # Use the loaded config to set up TrainingArguments
         training_args = TrainingArguments(
-            output_dir='./results',
-            num_train_epochs=3,
-            per_device_train_batch_size=16,
-            per_device_eval_batch_size=16,
-            warmup_steps=500,
-            weight_decay=0.01,
-            logging_dir='./logs'
+            output_dir=config['output_dir'],
+            num_train_epochs=config['num_train_epochs'],
+            per_device_train_batch_size=config['per_device_train_batch_size'],
+            per_device_eval_batch_size=config['per_device_eval_batch_size'],
+            warmup_steps=config['warmup_steps'],
+            weight_decay=config['weight_decay'],
+            logging_dir=config['logging_dir']
         )
 
         model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=len(train_data_tokenized['label'].unique()))
@@ -63,4 +78,5 @@ if __name__ == "__main__":
             eval_dataset=val_data_tokenized
         )
 
+        print("Training Model....")
         trainer.train()
